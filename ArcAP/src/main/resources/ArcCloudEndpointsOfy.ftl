bcc8634a51ac0de4f5ddb523408f3b9c5312abac
Ian Ni-Lewis <inl@google.com>
2014-04-14 08:54:51 -0700

    Maven-ify the annotation processor project

---
+++ ArcAP/src/main/resources/ArcCloudEndpointsOfy.ftl
@@ -0,0 +1,10 @@
+<#ftl>
+
+package com.github.inl1ne.fairlight.entities;
+
+/**
+* Created by ilewis on 2/13/14.
+*/
+public class Task2 {
+    String name;
+}
--- src/main/webapp/js/fairlightApp.js
+++
@@ -1,7 +0,0 @@
-var fairlightApp = angular.module('fairlightApp', []);
-
-fairlightApp.controller('fairlightControl', function ($scope, $http) {
-    $http.get('http://localhost:8080/_ah/api/fairlight/v1/users').success(function (data) {
-        $scope.users = data.results;
-    })
-});
